bc68e360fa5523d4e4dad5332f1bcf158d944b29
AkraichLand <54623031+AkraichLand@users.noreply.github.com>
2021-10-13 23:57:42 +0000

    Adding server-side page guards

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,17 +1,35 @@
-import type { NextPage } from 'next';
+import React from 'react';
+import type { NextPage, NextContext } from 'next';
 import Head from 'next/head';
-import Box from '@mui/material/Box';
+import { getSession } from 'next-auth/react';
 import UserProfile from '../components/UserProfile';
+import { GetServerSideProps } from 'next';
 
 const ProfilePage: NextPage = () => {
   return (
-    <Box>
+    <React.Fragment>
       <Head>
         <title>Profile Page</title>
       </Head>
       <UserProfile />
-    </Box>
+    </React.Fragment>
   );
 };
+
+export async function getServerSideProps(
+  context: NextContext
+): GetServerSideProps {
+  const session = await getSession({ req: context.req });
+  if (!session)
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      },
+    };
+  return {
+    props: { session },
+  };
+}
 
 export default ProfilePage;
